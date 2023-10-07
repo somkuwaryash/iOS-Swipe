446da5bed49161a42e3851b57dc0e6783bca9649
yash somkuwar <somkuwar.techfest@gmail.com>
2023-10-07 09:44:09 +0530

    Initial Commit

---
+++ assignment/assignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  assignmentApp.swift
+//  assignment
+//
+//  Created by Yash Somkuwar on 07/10/23.
+//
+
+import SwiftUI
+
+@main
+struct assignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
